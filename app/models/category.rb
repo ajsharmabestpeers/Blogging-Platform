7b5dec701d59ecb92d53bdd18a42a1aa09132312
Ajay Sharma <ajsharma@bestpeers.in>
2025-01-15 19:33:52 +0530

    Implemented title uniqueness validation

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,4 @@
 class Category < ApplicationRecord
-    validates :name, presence: true, uniqueness: true
+    validates :name, presence: true,uniqueness: { message: "Title must be unique" }
     has_many :posts
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,10 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness:{ message: "Title must be unique" }
+  
   validates :body, presence: true, length: { minimum: 10 }
   has_many :comments ,class_name: 'Comment',dependent: :destroy
   has_many_attached :post_images  
   belongs_to :category
+  
 end
